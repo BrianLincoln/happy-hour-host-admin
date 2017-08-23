f1d92bbc85e9f0b732e3e3f298b71e2f59e03459
Brian Lincoln <brianlincoln612@gmail.com>
2017-08-23 00:23:34 -0500

    moving form css

--- src/config.js
+++
@@ -1,4 +0,0 @@
-export default {
-    googleMapsApiKey: "AIzaSyDDbQf_9nZK-1G-P3X4dq21N1lMFedYIEs",
-    apiPath: process.env.NODE_ENV === "production" ? "http://happyhourhost.com" : "http://localhost"
-}
