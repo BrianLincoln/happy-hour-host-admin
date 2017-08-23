f25c61b97fc206f79b4c47d8d28cd0824c91c4b2
Brian Lincoln <brianlincoln612@gmail.com>
2017-08-22 23:02:13 -0500

    env specific api paths

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
     googleMapsApiKey: "AIzaSyDDbQf_9nZK-1G-P3X4dq21N1lMFedYIEs",
-    apiPath: "http://localhost:80"
+    apiPath: process.env.NODE_ENV === "production" ? "http://happyhourhost" : "http://localhost"
 }
